8909a291df7df08d8c09d6796a5418c8fbe54906
David Hicks <davidhsoftware@gmail.com>
2023-05-04 12:25:02 +1000

    Bump deps

--- packages/stream_chat_flutter/example/linux/flutter/generated_plugins.cmake
+++ packages/stream_chat_flutter/example/linux/flutter/generated_plugins.cmake
@@ -5,10 +5,8 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   dart_vlc
   desktop_drop
-  screen_retriever
-  sqlite3_flutter_libs
+  file_selector_linux
   url_launcher_linux
-  window_manager
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
